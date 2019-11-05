d1577b297835b5bc1fb8c439dbfa3ca135b5264c
mateuszmonas <mateuszmonas@gmail.com>
2019-11-05 11:35:00 +0100

    a

--- main/lab3/lab3.py
+++ main/lab3/lab3.py
@@ -7,39 +7,37 @@ def adj_list(g):
     graph = {}
     for e in g[1]:
         if e[0] - 1 not in graph:
-            graph[e[0] - 1] = {}
+            graph[e[0] - 1] = Node()
         if e[1] - 1 not in graph:
-            graph[e[1] - 1] = {}
+            graph[e[1] - 1] = Node()
         add_edge(graph, e[0] - 1, e[1] - 1, e[2])
     return graph
 
 
 def add_edge(graph, node_a, node_b, weight):
-    graph[node_a][node_b] = weight
-    graph[node_b][node_a] = weight
+    graph[node_a].add_edge(node_b, weight)
+    graph[node_b].add_edge(node_a, weight)
 
 
 def remove_edge(graph, node_a, node_b):
-    graph[node_a].pop(node_b)
-    graph[node_b].pop(node_a)
+    graph[node_a].remove_edge(node_b)
+    graph[node_b].remove_edge(node_a)
 
 
 def merge_vertices(graph, node_a, node_b):
-    cpy = graph[node_a].copy()
+    cpy = graph[node_a].edges.copy()
     for k, v in cpy.items():
         if k == node_b:
             remove_edge(graph, node_a, k)
             continue
-        if k in graph[node_b]:
-            graph[node_b][k] += v
-        else:
-            graph[node_b][k] = v
+        add_edge(graph, node_b, k, v)
         remove_edge(graph, node_a, k)
+
 
 
 def print_graph(graph):
     for k, v in graph.items():
-        print(k, v)
+        print(k, v.edges)
 
 
 def minimum_cut_phase(graph):
@@ -53,16 +51,16 @@ class Node:
         self.edges = {}
 
     def add_edge(self, to, weight):
-        self.edges[to]=weight
+        self.edges[to] = self.edges.get(to,0) + weight
 
     def remove_edge(self, to):
-        self.edges.pop(to)
+        del self.edges[to]
 
 
 def main():
     g = adj_list(load_weighted_graph('graphs-lab3/clique5'))
     merge_vertices(g, 0, 1)
-
+    print_graph(g)
 
 
 
